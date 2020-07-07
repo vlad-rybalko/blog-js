8a34dffc25cd654939ade96481f27765e1c826e5
Vadislav_Rybalko <vlad_rybalko@mail.ru>
2020-07-07 16:47:14 +0300

    Сделал навигацию. Работают табы

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -1,16 +1,20 @@
 import {Component} from '../core/component'
 
 export class HeaderComponent extends Component {
-    constructor(id) {
-        super(id)
-    }
+  constructor(id) {
+    super(id)
+  }
 
-    init() {
-        const btn = this.$el.querySelector('.js-header-start')
-        btn.addEventListener('click', buttonHandler.bind(this))
+  init() {
+    if (localStorage.getItem('visited')) {
+      this.hide()
     }
+    const btn = this.$el.querySelector('.js-header-start')
+    btn.addEventListener('click', buttonHandler.bind(this))
+  }
 }
 
 function buttonHandler() {
-    this.hide()
+  localStorage.setItem('visited', JSON.stringify(true))
+  this.hide()
 }
