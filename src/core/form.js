fe82c1d83e8e3dd7adb74aa566083cc0c485131b
Vadislav_Rybalko <vlad_rybalko@mail.ru>
2020-07-08 15:22:33 +0300

    Работает валидация

---
+++ src/core/form.js
@@ -0,0 +1,34 @@
+import { Validators } from './validators'
+
+export class Form {
+    constructor(form, controls) {
+        this.form = form
+        this.controls = controls
+    }
+
+    value() {
+        const value = {}
+
+        Object.keys(this.controls).forEach(control => {
+            value[control] = this.form[control].value
+        })
+        return value
+    }
+
+    isValid() {
+        let isFormValid = true
+
+        Object.keys(this.controls).forEach(control => {
+            const validators = this.controls[control]
+
+            let isValid = true
+            validators.forEach(validator => {
+                isValid = validator(this.form[control].value) && isValid
+            })
+
+            isFormValid = isFormValid && isValid
+        })
+
+        return isFormValid
+    }
+}
