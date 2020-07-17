7f2e4054ddd0ff1dab7a35d08c4af848489061e7
Vadislav_Rybalko <vlad_rybalko@mail.ru>
2020-07-17 16:17:43 +0300

    Загрузка данных в firebase

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,5 +1,6 @@
 import { Component } from '../core/component'
 import { Form } from '../core/form'
+import { apiService } from '../services/api.service'
 import { Validators } from '../core/validators'
 
 export class CreateComponent extends Component {
@@ -17,17 +18,18 @@ export class CreateComponent extends Component {
   }
 }
 
-function submitHandler(event) {
+async function submitHandler(event) {
   event.preventDefault()
 
   if (this.form.isValid()) {
     const formData = {
       type: this.$el.type.value,
+      date: new Date().toLocaleDateString(),
       ...this.form.value()
     }
 
+    await apiService.createPost(formData)
     this.form.clear()
-  
-    console.log('Submit', formData)
+    alert('Запись сохранена!')
   }
 }
