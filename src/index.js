7dc82ff15112d397e9f0ba85fb17d780e500f64b
Vadislav_Rybalko <vlad_rybalko@mail.ru>
2020-07-20 16:45:20 +0300

    Добавил индикатор загрузки

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import {NavigationComponent} from './components/navigation.component'
 import {CreateComponent} from './components/create.component'
 import {FavoriteComponent} from './components/favorite.component'
 import {PostsComponent} from './components/posts.component'
+import {LoaderComponent} from './components/loader.component';
 
 new HeaderComponent('header')
 
 const navigation = new NavigationComponent('navigation')
+const loader = new LoaderComponent('loader')
 
-const posts = new PostsComponent('posts')
+const posts = new PostsComponent('posts', {loader})
 const create = new CreateComponent('create')
 const favorite = new FavoriteComponent('favorite')
 
